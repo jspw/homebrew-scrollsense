596f5f1cdd4d2722cacdec3a387a2d783299f9bf
jspw <mhshifat757@gmail.com>
2026-04-04 13:51:01 +0600

    scrollsense 1.0.8

--- Formula/scrollsense.rb
+++ Formula/scrollsense.rb
@@ -1,8 +1,8 @@
 class Scrollsense < Formula
   desc "Automatically switches macOS natural scroll direction based on mouse or trackpad"
   homepage "https://github.com/jspw/ScrollSense"
-  url "https://github.com/jspw/ScrollSense/archive/refs/tags/v1.0.7.tar.gz"
-  sha256 "585aedf7016f6fa6b48d383c6beb7ca2b9fe4d993129d338958b90b883132afc"
+  url "https://github.com/jspw/ScrollSense/archive/refs/tags/v1.0.8.tar.gz"
+  sha256 "2e5c6624184c8b8a8decc6a6663f07293f6d2f79caeba2a36162cbf70d7a2c2f"
   license "MIT"
 
   depends_on xcode: ["14.0", :build]
@@ -39,6 +39,6 @@ class Scrollsense < Formula
   end
 
   test do
-    assert_match "1.0.7", shell_output("#{bin}/scrollSense --version")
+    assert_match "1.0.8", shell_output("#{bin}/scrollSense --version")
   end
 end
